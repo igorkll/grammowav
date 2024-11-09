c8c911c92d10e95419bef53227af256b726d083e
igorkll <frerther@gmail.com>
2024-11-09 12:49:59 +0300

    sdf

--- grammowav.cpp
+++ grammowav.cpp
@@ -1,4 +1,32 @@
 #include "microstl.h"
+#include "stdbool.h"
+#include "stdint.h"
+
+static double convertSample(uint8_t* datapart, size_t size, bool signedInt) {
+	switch (size) {
+		case 1:
+			if (signedInt) {
+				return *((int8_t*)datapart) / 128.0;
+			} else {
+				return (*((uint8_t*)datapart) - 127.0) / 128.0;
+			}
+
+		case 2:
+			if (signedInt) {
+				return *((int16_t*)datapart) / 32768.0;
+			} else {
+				return (*((uint16_t*)datapart) - 32767.0) / 32768.0;
+			}
+
+		case 4:
+			if (signedInt) {
+				return *((int32_t*)datapart) / 2147483648.0;
+			} else {
+				return (*((uint32_t*)datapart) - 2147483647.0) / 2147483648.0;
+			}
+	}
+	return 0;
+}
 
 extern "C" int grammowav_wavToStl(const char* path, const char* exportPath, double rpm, double diskSize, double trackWidth, double trackAmplitude) {
 	/*
@@ -49,18 +77,30 @@ extern "C" int grammowav_wavToStl(const char* path, const char* exportPath, doub
 	char subchunk2Id[4];
 	fread(&subchunk2Id, 4, 1, file);
 
-	uint32_t subchunk2Size;
-	fread(&subchunk2Size, 4, 1, file);
+	uint32_t offset;
+	fread(&offset, 4, 1, file);
 
-	if (byteRate > 4) byteRate = 4;
+	size_t rate = bitsPerSample / 8;
+	if (strcmp(subchunk2Id, "data") != 0) {
+		fseek(file, offset, SEEK_CUR);
+		char title[4];
+		fread(&title, 1, 4, file);
+		fread(&offset, 4, 1, file);
+	}
+	size_t fileSize = offset;
 
 	size_t currentOffset = 0;
+	uint8_t datapart[4];
 	while (true) {
-		uint8_t datapart[4];
-		fread(datapart, 1, byteRate, file);
+		double sample = 0;
+		for (size_t channel = 0; channel < numChannels; channel++) {
+			fread(datapart, 1, rate, file);
+			sample += convertSample(datapart, rate, false);
+		}
+		sample /= numChannels;
 		
-		currentOffset += byteRate;
-		if (currentOffset >= subchunk2Size) break;
+		currentOffset += rate * numChannels;
+		if (currentOffset >= fileSize) break;
 	}
 
 	return 0;
