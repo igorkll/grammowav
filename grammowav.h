dcff6d2327a4b06f7ea7a091551438526609fb5b
igorkll <frerther@gmail.com>
2024-11-16 10:21:09 +0300

    backup

--- grammowav.h
+++ grammowav.h
@@ -143,6 +143,7 @@ int grammowav_wavToGcode(const char* path, const char* exportPath, printer_t pri
 	}
 	gcode_extrusion = false;
 
+	// читаю ВЕСЬ wav в оперативу (сам знаю что дофига весить будет, но мне сейчас не до оптимизации)
 	double* soundData = malloc(samplesCount * sizeof(double));
 	size_t currentSample = 0;
 	uint8_t datapart[4];
@@ -156,6 +157,29 @@ int grammowav_wavToGcode(const char* path, const char* exportPath, printer_t pri
 		soundData[currentSample] = sample / numChannels;
 		currentSample++;
 		if (currentSample >= samplesCount) break;
+	}
+
+	// нармализую звук, деля его на фрагменты а потом подбирая множитель пока не упреться в предел
+	int normalize_frame = sampleRate / 8;
+	double normalize_step = 0.05;
+	double normalize_mulUp = 1 + normalize_step;
+	double normalize_mulDown = 1 - normalize_step;
+
+	for (size_t offset = 0; offset < samplesCount; offset += normalize_frame) {
+		while (true) {
+			bool overrange = false;
+			for (size_t frameOffset = 0; frameOffset < normalize_frame; frameOffset++) {
+				soundData[offset + frameOffset] *= normalize_mulUp;
+				double value = soundData[offset + frameOffset];
+				if (value > 1 || value < -1) overrange = true;
+			}
+			if (overrange) {
+				for (size_t frameOffset = 0; frameOffset < normalize_frame; frameOffset++) {
+					soundData[offset + frameOffset] *= normalize_mulDown;
+				}
+				break;
+			}
+		}
 	}
 	
 	// фигачу дорожку
